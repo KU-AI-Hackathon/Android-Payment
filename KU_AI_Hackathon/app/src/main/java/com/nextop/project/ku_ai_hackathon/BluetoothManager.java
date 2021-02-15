0135ee5933114e8a93f21424681994e59e2a73bd
민상연 <judemin2087@naver.com>
2021-02-15 22:52:13 +0900

    feat : Bluetooth connection
    Bluetooth manager function

---
+++ KU_AI_Hackathon/app/src/main/java/com/nextop/project/ku_ai_hackathon/BluetoothManager.java
@@ -0,0 +1,342 @@
+package com.nextop.project.ku_ai_hackathon;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class BluetoothManager {
+
+    private final int REQUEST_BLUETOOTH_ENABLE = 100;
+
+    private Activity mActivity;
+
+    ConnectedTask mConnectedTask = null;
+    static BluetoothAdapter mBluetoothAdapter;
+    private String mConnectedDeviceName = null;
+    static boolean isConnectionError = false;
+    private static final String TAG = "BluetoothClient";
+
+    public BluetoothManager(Activity _activity){
+        mActivity = _activity;
+
+
+        Log.d( TAG, "Initalizing Bluetooth adapter...");
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            showErrorDialog("This device is not implement Bluetooth.");
+            return;
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            mActivity.startActivityForResult(intent,0);
+        }
+        else {
+            Log.d(TAG, "Initialisation successful.");
+
+            showPairedDevicesListDialog();
+        }
+    }
+
+    public void destroy() {
+
+        if ( mConnectedTask != null ) {
+            mConnectedTask.cancel(true);
+        }
+    }
+
+
+    private class ConnectTask extends AsyncTask<Void, Void, Boolean> {
+
+        private BluetoothSocket mBluetoothSocket = null;
+        private BluetoothDevice mBluetoothDevice = null;
+
+        ConnectTask(BluetoothDevice bluetoothDevice) {
+            mBluetoothDevice = bluetoothDevice;
+            mConnectedDeviceName = bluetoothDevice.getName();
+
+            //SPP
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+            try {
+                mBluetoothSocket = mBluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+                Log.d( TAG, "create socket for "+mConnectedDeviceName);
+
+            } catch (IOException e) {
+                Log.e( TAG, "socket create failed " + e.getMessage());
+            }
+
+            Log.d( TAG,  "connecting...");
+        }
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            // Always cancel discovery because it will slow down a connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            // Make a connection to the BluetoothSocket
+            try {
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                mBluetoothSocket.connect();
+            } catch (IOException e) {
+                // Close the socket
+                try {
+                    mBluetoothSocket.close();
+                } catch (IOException e2) {
+                    Log.e(TAG, "unable to close() " +
+                            " socket during connection failure", e2);
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+
+        @Override
+        protected void onPostExecute(Boolean isSucess) {
+
+            if ( isSucess ) {
+                connected(mBluetoothSocket);
+            }
+            else{
+
+                isConnectionError = true;
+                Log.d( TAG,  "Unable to connect device");
+                showErrorDialog("Unable to connect device");
+            }
+        }
+    }
+
+
+    public void connected( BluetoothSocket socket ) {
+        mConnectedTask = new ConnectedTask(socket);
+        mConnectedTask.execute();
+    }
+
+
+    public void showPairedDevicesListDialog()
+    {
+        Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+        final BluetoothDevice[] pairedDevices = devices.toArray(new BluetoothDevice[0]);
+
+        if ( pairedDevices.length == 0 ){
+            showQuitDialog( "No devices have been paired.\n"
+                    +"You must pair it with another device.");
+            return;
+        }
+
+        String[] items;
+        items = new String[pairedDevices.length];
+        for (int i=0;i<pairedDevices.length;i++) {
+            items[i] = pairedDevices[i].getName();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        builder.setTitle("Select device");
+        builder.setCancelable(false);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+                ConnectTask task = new ConnectTask(pairedDevices[which]);
+                task.execute();
+            }
+        });
+        builder.create().show();
+    }
+
+
+
+    public void showErrorDialog(String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        builder.setTitle("Quit");
+        builder.setCancelable(false);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK",  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if ( isConnectionError  ) {
+                    isConnectionError = false;
+                    mActivity.finish();
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+
+    public void showQuitDialog(String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        builder.setTitle("Quit");
+        builder.setCancelable(false);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK",  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                mActivity.finish();
+            }
+        });
+        builder.create().show();
+    }
+
+    void sendMessage(String msg){
+
+        if ( mConnectedTask != null ) {
+            Log.d(TAG, "send message: " + msg);
+        }
+    }
+
+    private class ConnectedTask extends AsyncTask<Void, String, Boolean> {
+
+        private InputStream mInputStream = null;
+        private OutputStream mOutputStream = null;
+        private BluetoothSocket mBluetoothSocket = null;
+
+        ConnectedTask(BluetoothSocket socket){
+
+            mBluetoothSocket = socket;
+            try {
+                mInputStream = mBluetoothSocket.getInputStream();
+                mOutputStream = mBluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "socket not created", e );
+            }
+
+            Log.d( TAG, "connected to "+mConnectedDeviceName);
+        }
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            byte [] readBuffer = new byte[1024];
+            int readBufferPosition = 0;
+
+
+            while (true) {
+
+                if ( isCancelled() ) return false;
+
+                try {
+
+                    int bytesAvailable = mInputStream.available();
+
+                    if(bytesAvailable > 0) {
+
+                        byte[] packetBytes = new byte[bytesAvailable];
+
+                        mInputStream.read(packetBytes);
+
+                        for(int i=0;i<bytesAvailable;i++) {
+
+                            byte b = packetBytes[i];
+                            if(b == '\n')
+                            {
+                                byte[] encodedBytes = new byte[readBufferPosition];
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0,
+                                        encodedBytes.length);
+                                String recvMessage = new String(encodedBytes, "UTF-8");
+
+                                readBufferPosition = 0;
+
+                                Log.d(TAG, "recv message: " + recvMessage);
+                                publishProgress(recvMessage);
+                            }
+                            else
+                            {
+                                readBuffer[readBufferPosition++] = b;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+
+                    Log.e(TAG, "disconnected", e);
+                    return false;
+                }
+            }
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... recvMessage) {
+            Log.d(TAG, mConnectedDeviceName + ": " + recvMessage[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isSucess) {
+            super.onPostExecute(isSucess);
+
+            if ( !isSucess ) {
+
+
+                closeSocket();
+                Log.d(TAG, "Device connection was lost");
+                isConnectionError = true;
+                showErrorDialog("Device connection was lost");
+            }
+        }
+
+        @Override
+        protected void onCancelled(Boolean aBoolean) {
+            super.onCancelled(aBoolean);
+
+            closeSocket();
+        }
+
+        void closeSocket(){
+
+            try {
+
+                mBluetoothSocket.close();
+                Log.d(TAG, "close socket()");
+
+            } catch (IOException e2) {
+
+                Log.e(TAG, "unable to close() " +
+                        " socket during connection failure", e2);
+            }
+        }
+
+        void write(String msg){
+
+            msg += "\n";
+
+            try {
+                mOutputStream.write(msg.getBytes());
+                mOutputStream.flush();
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during send", e );
+            }
+        }
+    }
+}
+
+
--- KU_AI_Hackathon/app/src/main/java/com/nextop/project/ku_ai_hackathon/MainActivity.java
+++ KU_AI_Hackathon/app/src/main/java/com/nextop/project/ku_ai_hackathon/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Config;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,7 +39,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     BillingManager bm;
-
+    BluetoothManager blem;
     private String GP_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjGjZqpS72P+GJQ7Y0pDDILKZ8H/ITCLS+8NT58RXNS5Yd78JwEnJ3mzgYeyBEukZyMCeoJ6QnepRvYo9qm1gzEHeGtp2RsA8LV0G22wi+IM65Wlfo7736ml8okLNBCUGjlBp9B7fxf6LDFeN7Vu0SVH9DOqMTnJwJX02shY8ms5w0Dy8K/CMvNEAJopxGxATuic631qLzhUUZvHuIDby8tN9DgvznmI/0o20hTs9+IowiMkLyTQF/7bG23dtNcPqBQoJnh8IHLK7QR5L4xMIGSMDc7ENA0Ho885L5GYFvGB9ZiR6V4Qdi/bxyWApwUvfSr401w7CyHOs4LDufXMnRQIDAQAB";
 
     //
@@ -55,16 +58,37 @@ public class MainActivity extends AppCompatActivity {
         ib = findViewById(R.id.imageButton2);
 
         bm = new BillingManager(this, this);
+        blem = new BluetoothManager(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        blem.destroy();
     }
 
     //
 
-    public void purchase(View view){
+    public void pairing(View view){
+
+        //success
+        Toast.makeText(this, "자판기와 연결되었습니다.", Toast.LENGTH_SHORT).show();
+        ib.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                purchase(0);
+            }
+        });
+        ib.setImageResource(R.drawable.mask_1);
+        tv.setText("마스크를 구매하시려면 화면을 터치하세요.");
+    }
+
+    //
+
+    public void purchase(int index){
         //bm.get_Sku_Detail_List();
-        bm.purchase(0);
+        bm.purchase(index);
     }
-
-    //
 
     public void purchaseSuccess(){
         Toast.makeText(this, "마스크 구매에 성공했습니다.", Toast.LENGTH_SHORT).show();
